24cad1a7a1c2629bf8becd70a1701955d27620a0
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-05 13:48:52 +0000

    Add item

--- spigot/src/main/java/com/github/mcengine/MCEngineArtifact.java
+++ spigot/src/main/java/com/github/mcengine/MCEngineArtifact.java
@@ -2,10 +2,14 @@ package com.github.mcengine;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.mcengine.ashar.asharItem;
+
 public class MCEngineArtifact extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Register our item
+        asharItem.sword();
         // Register our command
         getCommand("ashar").setExecutor(new asharCommand());
     }
