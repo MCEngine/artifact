284f7b1b56f764cd2ea30ddc004f35faee7b55a1
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-05 13:42:07 +0000

    Create asharCommand.java

---
+++ spigot/src/main/java/com/github/mcengine/ashar/asharCommand.java
@@ -0,0 +1,41 @@
+package com.github.mcengine.ashar;
+
+import com.github.mcengine.ashar.item;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class command implements CommandExecutor{
+    @Override
+    public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // Check if sender is a player
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+            return;
+        }
+
+        // Check player permission
+        if (!sender.hasPermission("op")) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+            return;
+        }
+
+        // Check arguments range
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Usage: /ashar <itemType>");
+            return;
+        }
+
+        // Check if arg[0] == "get" and arg[1] == "sword"
+        if (args[0].equalsIgnoreCase("get") && args[1].equalsIgnoreCase("sword")) {
+            Player player = (Player) sender;
+            player.getInventory().addItem(item.sword);
+            return;
+        }
+
+        return;
+    }
+}
