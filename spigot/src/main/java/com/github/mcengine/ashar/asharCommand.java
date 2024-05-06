fb97dfad80d2e13c0137cf5488dc829457d02848
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-06 01:52:39 +0000

    Fix invalid function type

--- spigot/src/main/java/com/github/mcengine/ashar/asharCommand.java
+++ spigot/src/main/java/com/github/mcengine/ashar/asharCommand.java
@@ -1,7 +1,5 @@
 package com.github.mcengine.ashar;
 
-import com.github.mcengine.ashar.item;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,32 +8,32 @@ import org.bukkit.entity.Player;
 
 public class asharCommand implements CommandExecutor{
     @Override
-    public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         // Check if sender is a player
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "Only players can use this command!");
-            return;
+            return true;
         }
 
         // Check player permission
         if (!sender.hasPermission("op")) {
             sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
-            return;
+            return true;
         }
 
         // Check arguments range
         if (args.length < 1) {
             sender.sendMessage(ChatColor.RED + "Usage: /ashar <itemType>");
-            return;
+            return true;
         }
 
         // Check if arg[0] == "get" and arg[1] == "sword"
         if (args[0].equalsIgnoreCase("get") && args[1].equalsIgnoreCase("sword")) {
             Player player = (Player) sender;
             player.getInventory().addItem(asharItem.sword);
-            return;
+            return true;
         }
 
-        return;
+        return true;
     }
 }
