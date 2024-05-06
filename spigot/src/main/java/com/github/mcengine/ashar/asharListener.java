d523b4436a67d84dac96cebc7860b38e46376fcf
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-06 02:22:48 +0000

    Create asharListener.java

---
+++ spigot/src/main/java/com/github/mcengine/ashar/asharListener.java
@@ -0,0 +1,34 @@
+package com.github.mcengine.ashar;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.block.Action;
+
+public class asharListener implements Listener {
+    // If player right click item name ChatColor.GOLD + "Sword of the God"
+    // It will send message to player Lightining Strike
+    // make it stike to 5 enemy around player
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (item == null) return;
+        if (!item.hasItemMeta()) return;
+        if (!item.getItemMeta().hasDisplayName()) return;
+        if (!item.getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Sword of the God")) return;
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        player.sendMessage(ChatColor.RED + "Lightning Strike!");
+        player.getWorld().strikeLightning(player.getLocation());
+        for (Entity entity : player.getNearbyEntities(5, 5, 5)) {
+            if (entity instanceof LivingEntity) {
+                ((LivingEntity) entity).damage(5);
+            }
+        }
+    }
+}
