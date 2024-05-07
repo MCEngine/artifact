83fdae4fe644c2ea155a5ad0dba47d52ca2c0e92
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-07 21:58:03 +0000

    Change path

--- spigot/src/main/java/com/github/mcengine/commands/asharCommand.java
+++ spigot/src/main/java/com/github/mcengine/commands/asharCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.github.mcengine.ashar.asharItem;
+import com.github.mcengine.items.asharItem;
 
 public class asharCommand implements CommandExecutor{
     @Override
