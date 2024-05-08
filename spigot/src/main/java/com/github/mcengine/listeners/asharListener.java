b230fb971c1b4bbfb6069c46b704ac316178c629
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-08 00:04:10 +0000

    Change method to another file

--- spigot/src/main/java/com/github/mcengine/listeners/asharListener.java
+++ spigot/src/main/java/com/github/mcengine/listeners/asharListener.java
@@ -45,27 +45,4 @@ public class asharListener implements Listener {
         }
         setCooldown(playerUUID);
     }
-
-    private boolean hasCooldown(UUID playerUUID) {
-        if (cooldowns.containsKey(playerUUID)) {
-            long cooldownTime = cooldowns.get(playerUUID);
-            if (System.currentTimeMillis() < cooldownTime + cooldownDuration) {
-                return true;
-            } else {
-                cooldowns.remove(playerUUID);
-            }
-        }
-        return false;
-    }
-
-    private void setCooldown(UUID playerUUID) {
-        cooldowns.put(playerUUID, System.currentTimeMillis());
-    }
-
-    private long getRemainingCooldown(UUID playerUUID) {
-        long cooldownTime = cooldowns.get(playerUUID);
-        long currentTime = System.currentTimeMillis();
-        long remainingTime = cooldownTime + cooldownDuration - currentTime;
-        return Math.max(0, remainingTime);
-    }
 }
